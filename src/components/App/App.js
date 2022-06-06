af324dd0bd6b09d6ac53a80da49e0204935e750c
Katya Ksenofontova <dkurach@rfc1918.private.ip.localhost>
2022-06-06 17:25:38 +0300

    fixed burger menu

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from '../Header/Header.js';
 import Footer from '../Footer/Footer.js';
+import NewProduct from '../NewProduct/NewProduct';
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div className="app">
       <div className='app__container'>
         <Header />
+        <NewProduct />
         <Footer />
       </div>
     </div>
--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Burger.css';
+import '../App/App.css';
 
 function Burger() {
     const [isBurgerOpen, setIsBurgerOpen] = React.useState(false)
@@ -14,19 +15,23 @@ function Burger() {
 
     return (
         <>
-            <button type='button' className='burger__btn' onClick={handleOpenBurgerClick} />
+            <div className='burger__btn' onClick={handleOpenBurgerClick}>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
             <div className={`burger ${isBurgerOpen ? 'burger_is-opened' : ''}`}>
                 <div className='burger__container'>
                     <button type='button' className='burger__btn-close' onClick={handleCloseBurgerClick} />
-                    <nav className='burger__catalog'>Оборудование
-                        <p className='burger__catalog-link'>Свет</p>
-                        <p className='burger__catalog-link'>Свет</p>
-                        <p className='burger__catalog-link'>Свет</p>
-                        <p className='burger__catalog-link'>Свет</p>
-                        <p className='burger__catalog-link'>Свет</p>
-                        <p className='burger__catalog-link'>Свет</p>
-                        <div className='burger__contacts'>Контакты</div>
-                    </nav>
+                        <nav className='burger__catalog'>Оборудование
+                            <p className='burger__catalog-link'>Свет</p>
+                            <p className='burger__catalog-link'>Свет</p>
+                            <p className='burger__catalog-link'>Свет</p>
+                            <p className='burger__catalog-link'>Свет</p>
+                            <p className='burger__catalog-link'>Свет</p>
+                            <p className='burger__catalog-link'>Свет</p>
+                            <div className='burger__contacts'>Контакты</div>
+                        </nav>
                 </div>
             </div>
         </>
