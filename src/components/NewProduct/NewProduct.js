64c437162dae72fcc1ddf39970288e7536003dcf
Katya Ksenofontova <dkurach@rfc1918.private.ip.localhost>
2022-06-07 14:52:31 +0300

    add scroll-bar

--- src/components/NewProduct/NewProduct.js
+++ src/components/NewProduct/NewProduct.js
@@ -5,22 +5,37 @@ import '../App/App.css';
 function NewProduct() {
     return (
         <section className="new">
-            <h2 className="new__title">Наши новинки</h2>
+            {/* <h2 className="new__title">Новинки</h2> */}
             <ul className="new__list list">
                 <li className="new__item">
                     <p className="new__item-head">New</p>
                     <h3 className="new__item-title">Камераблабла</h3>
-                    <p className="new__item-subtitle">О Камереблабла</p>
+                    <p className="new__item-subtitle">О kамереблабла</p>
                 </li>
                 <li className="new__item">
                     <p className="new__item-head">New</p>
                     <h3 className="new__item-title">Камераблабла</h3>
-                    <p className="new__item-subtitle">О Камереблабла</p>
+                    <p className="new__item-subtitle">О kамереблабла</p>
                 </li>
                 <li className="new__item">
                     <p className="new__item-head">New</p>
                     <h3 className="new__item-title">Камераблабла</h3>
-                    <p className="new__item-subtitle">О Камереблабла</p>
+                    <p className="new__item-subtitle">О kамереблабла</p>
+                </li>
+                <li className="new__item">
+                    <p className="new__item-head">New</p>
+                    <h3 className="new__item-title">Камераблабла</h3>
+                    <p className="new__item-subtitle">О kамереблабла</p>
+                </li>
+                <li className="new__item">
+                    <p className="new__item-head">New</p>
+                    <h3 className="new__item-title">Камераблабла</h3>
+                    <p className="new__item-subtitle">О kамереблабла</p>
+                </li>
+                <li className="new__item">
+                    <p className="new__item-head">New</p>
+                    <h3 className="new__item-title">Камераблабла</h3>
+                    <p className="new__item-subtitle">О kамереблабла</p>
                 </li>
             </ul>
         </section>
