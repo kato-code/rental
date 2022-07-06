493e5e20c04e50e9146d56b9014b884105589abb
Katya Ksenofontova <dkurach@rfc1918.private.ip.localhost>
2022-07-06 17:34:22 +0300

    basic markup

--- src/components/Catalog/Catalog.js
+++ src/components/Catalog/Catalog.js
@@ -8,46 +8,52 @@ function Catalog() {
             <h2 className="catalog__title">АРЕНДА</h2>
             <ul className="catalog__list list" id="link">
                 <li className="catalog__item">
-                    {/* <a href="#" className="catalog__item-link"></a> */}
-                    <div className="catalog__item-container">
-                        <h3 className="catalog__item-title">Камеры</h3>
-                        <div className="catalog__item-arrow"></div>
-                    </div>
+                    <a className="catalog__item-link" href="#link">
+                        <div className="catalog__item-container">
+                            <h3 className="catalog__item-title">Камеры</h3>
+                            <div className="catalog__item-arrow"></div>
+                        </div>
+                    </a>
                 </li>
                 <li className="catalog__item">
-                    {/* <a href="#" className="catalog__item-link"></a> */}
-                    <div className="catalog__item-container">
-                        <h3 className="catalog__item-title">Оптика</h3>
-                        <div className="catalog__item-arrow"></div>
-                    </div>
+                    <a className="catalog__item-link" href="#link">
+                        <div className="catalog__item-container">
+                            <h3 className="catalog__item-title">Оптика</h3>
+                            <div className="catalog__item-arrow"></div>
+                        </div>
+                    </a>
                 </li>
                 <li className="catalog__item">
-                    {/* <a href="#" className="catalog__item-link"></a> */}
-                    <div className="catalog__item-container">
-                        <h3 className="catalog__item-title">Световое оборудование</h3>
-                        <div className="catalog__item-arrow"></div>
-                    </div>
+                    <a className="catalog__item-link" href="#link">
+                        <div className="catalog__item-container">
+                            <h3 className="catalog__item-title">Световое оборудование</h3>
+                            <div className="catalog__item-arrow"></div>
+                        </div>
+                    </a>
                 </li>
                 <li className="catalog__item">
-                    {/* <a href="#" className="catalog__item-link"></a> */}
-                    <div className="catalog__item-container">
-                        <h3 className="catalog__item-title">Стабилизация</h3>
-                        <div className="catalog__item-arrow"></div>
-                    </div>
+                    <a className="catalog__item-link" href="#link">
+                        <div className="catalog__item-container">
+                            <h3 className="catalog__item-title">Стабилизация</h3>
+                            <div className="catalog__item-arrow"></div>
+                        </div>
+                    </a>
                 </li>
                 <li className="catalog__item">
-                    {/* <a href="#" className="catalog__item-link"></a> */}
-                    <div className="catalog__item-container">
-                        <h3 className="catalog__item-title">Видеосендеры и мониторы</h3>
-                        <div className="catalog__item-arrow"></div>
-                    </div>
+                    <a className="catalog__item-link" href="#link">
+                        <div className="catalog__item-container">
+                            <h3 className="catalog__item-title">Видеосендеры и мониторы</h3>
+                            <div className="catalog__item-arrow"></div>
+                        </div>
+                    </a>
                 </li>
                 <li className="catalog__item">
-                    {/* <a href="#" className="catalog__item-link"></a> */}
-                    <div className="catalog__item-container">
-                        <h3 className="catalog__item-title">Дополнительное оборудование</h3>
-                        <div className="catalog__item-arrow"></div>
-                    </div>
+                    <a className="catalog__item-link" href="#link">
+                        <div className="catalog__item-container">
+                            <h3 className="catalog__item-title">Дополнительное оборудование</h3>
+                            <div className="catalog__item-arrow"></div>
+                        </div>
+                    </a>
                 </li>
             </ul>
         </section>
