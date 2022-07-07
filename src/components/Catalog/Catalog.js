e15a2fe7d1c789a3c9a3bf3c372dbd4fb773c4a0
Katya Ksenofontova <dkurach@rfc1918.private.ip.localhost>
2022-07-07 15:56:05 +0300

    add 4 blocks

--- src/components/Catalog/Catalog.js
+++ src/components/Catalog/Catalog.js
@@ -1,61 +1,17 @@
 import React from "react";
 import './Catalog.css';
-import '../App/App.css';
+import Cameras from '../Cameras/Cameras.js';
+import Optics from '../Optics/Optics.js';
+import Additionally from "../Additionally/Additionally";
+import Monitors from "../Monitors/Monitors";
 
 function Catalog() {
     return (
         <section className="catalog">
-            <h2 className="catalog__title">АРЕНДА</h2>
-            <ul className="catalog__list list" id="link">
-                <li className="catalog__item">
-                    <a className="catalog__item-link" href="#link">
-                        <div className="catalog__item-container">
-                            <h3 className="catalog__item-title">Камеры</h3>
-                            <div className="catalog__item-arrow"></div>
-                        </div>
-                    </a>
-                </li>
-                <li className="catalog__item">
-                    <a className="catalog__item-link" href="#link">
-                        <div className="catalog__item-container">
-                            <h3 className="catalog__item-title">Оптика</h3>
-                            <div className="catalog__item-arrow"></div>
-                        </div>
-                    </a>
-                </li>
-                <li className="catalog__item">
-                    <a className="catalog__item-link" href="#link">
-                        <div className="catalog__item-container">
-                            <h3 className="catalog__item-title">Световое оборудование</h3>
-                            <div className="catalog__item-arrow"></div>
-                        </div>
-                    </a>
-                </li>
-                <li className="catalog__item">
-                    <a className="catalog__item-link" href="#link">
-                        <div className="catalog__item-container">
-                            <h3 className="catalog__item-title">Стабилизация</h3>
-                            <div className="catalog__item-arrow"></div>
-                        </div>
-                    </a>
-                </li>
-                <li className="catalog__item">
-                    <a className="catalog__item-link" href="#link">
-                        <div className="catalog__item-container">
-                            <h3 className="catalog__item-title">Видеосендеры и мониторы</h3>
-                            <div className="catalog__item-arrow"></div>
-                        </div>
-                    </a>
-                </li>
-                <li className="catalog__item">
-                    <a className="catalog__item-link" href="#link">
-                        <div className="catalog__item-container">
-                            <h3 className="catalog__item-title">Дополнительное оборудование</h3>
-                            <div className="catalog__item-arrow"></div>
-                        </div>
-                    </a>
-                </li>
-            </ul>
+            <Cameras />
+            <Optics />
+            <Monitors />
+            <Additionally />
         </section>
     )
 }
